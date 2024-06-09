8e24f80da0fcef40bfb3fb1b6162b96b0e1d0c44
seasonedgarlic <aefraim@yandex.com>
2024-06-09 12:41:26 +0300

    Update module2hard.py
    исправил добавление с пар на отдельные числа

--- module2hard.py
+++ module2hard.py
@@ -6,6 +6,7 @@ result = []
 for i in range(len(pairs)):
   for j in range(i + 1, len(pairs)):
     if n % (pairs[i] + pairs[j]) == 0:
-      result.append((pairs[i], pairs[j]))
+      result.append(pairs[i])
+      result.append(pairs[j]) #не добавляю парами, чтобы на выводе легче читалось
     
 print(f'{n} - {result}') 
