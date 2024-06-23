f34da7cc73772e78cb6a4b4e28cdb9b73d397bb3
seasonedgarlic <aefraim@yandex.com>
2024-06-23 17:49:37 +0300

    Update module_5.3.py

--- module_5.3.py
+++ module_5.3.py
@@ -2,8 +2,9 @@ class Building:
     def __init__(self, numberOfFloors, buildingType):
         self.numberOfFloors = int(numberOfFloors)
         self.buildingType = str(buildingType)
-    def __eq__(self):
-        return self.numberOfFloors == self.buildingType
+    def __eq__(self, other):
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
 h1 = Building(10, 'barn')
-print(h1.__eq__())
+h2 = Building(3, 'barn')
+print(h1 == h2)
